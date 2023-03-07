c40491fa948087c7b678ccd61982d665f745b4b3
양소연 <yangsoyeon@yangsoyeon-ui-MacBookPro-2.local>
2023-03-08 05:03:27 +0900

    feat: 라우트 설정 및 루트 경로로 접근시 리다이렉트 되도록 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import ProductPage from './pages/product/ProductPage';
+import CartPage from './pages/cart/CartPage';
+
 const Router = () => {
-  return <div>Router</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/main" />} />
+        <Route path="/main" element={<ProductPage />} />
+        <Route path="/reservations" element={<CartPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
