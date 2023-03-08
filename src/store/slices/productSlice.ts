748ffc9e8abff8080f78184a5688144524cc704e
JUNU <bokjunwoo@gmail.com>
2023-03-08 19:21:03 +0900

    feat : productSlice - getFilteredProducts 함수로 금액에 대한 필터링 기능 구현
    getProducts 액션은 모든 데이터를 처음에 로딩해줍니다.
    getFilteredProducts 액션은 필터링 된 데이터를 로딩해줍니다.

--- src/store/slices/productSlice.ts
+++ src/store/slices/productSlice.ts
@@ -2,27 +2,34 @@ import productApi from '@/api/product';
 import { IProduct, IProductReducer } from '@/interface/product';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
+interface IFilterState {
+  minPrice: number;
+  maxPrice: number;
+}
+
 export const getProducts = createAsyncThunk<IProduct[]>(
   'product/getProducts',
   productApi.getProducts,
 );
 
+export const getFilteredProducts = createAsyncThunk(
+  'product/getFilteredProducts',
+  async ({ minPrice, maxPrice }: IFilterState) => {
+    const response = await productApi.getProducts();
+    const filteredProducts = response.filter(
+      (product: IProduct) =>
+        Number(product.price) >= minPrice && Number(product.price) <= maxPrice,
+    );
+    return filteredProducts;
+  },
+);
+
 const initialState: IProductReducer = {
   isLoading: true,
   error: null,
-  products: [
-    {
-      idx: '',
-      name: '',
-      mainImage: '',
-      description: '',
-      spaceCategory: '',
-      price: '',
-      maximumPurchases: '',
-      registrationDate: '',
-    },
-  ],
+  products: [],
 };
+
 const productSlice = createSlice({
   name: 'products',
   initialState,
@@ -40,6 +47,19 @@ const productSlice = createSlice({
     builder.addCase(getProducts.rejected, (state) => {
       state.isLoading = false;
       state.error = '상품 목록을 가져올 수 없습니다.';
+    });
+    builder.addCase(getFilteredProducts.pending, (state) => {
+      state.isLoading = true;
+      state.error = null;
+    });
+    builder.addCase(getFilteredProducts.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.error = null;
+      state.products = action.payload;
+    });
+    builder.addCase(getFilteredProducts.rejected, (state) => {
+      state.isLoading = false;
+      state.error = '찾은 목록을 가져올 수 없습니다.';
     });
   },
 });
