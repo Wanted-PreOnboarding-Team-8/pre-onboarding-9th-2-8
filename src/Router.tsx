d73e37ca1c57729049598db71eef0750a41c658c
JUNU <bokjunwoo@gmail.com>
2023-03-09 14:21:17 +0900

    feat : <Loading>컴포넌트 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
-import { Spinner } from '@chakra-ui/react';
 import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Loading from './components/Loading';
 
 const Main = lazy(() => import('@/pages/Main'));
 const Reservations = lazy(() => import('@/pages/Reservations'));
@@ -8,17 +8,7 @@ const Reservations = lazy(() => import('@/pages/Reservations'));
 const Router = () => {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Suspense
-        fallback={
-          <Spinner
-            thickness="4px"
-            speed="0.65s"
-            emptyColor="gray.200"
-            color="blue.500"
-            size="xl"
-          />
-        }
-      >
+      <Suspense fallback={<Loading text="페이지 로딩중..." />}>
         <Routes>
           <Route path="/" element={<Navigate to="/main" />} />
           <Route path="/main" element={<Main />} />
