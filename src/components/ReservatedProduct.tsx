a9e4371b667d5763dcbd687cbbfb4d589dbb92ed
sangyeon kim <greyhairchooselife@gmail.com>
2023-03-10 11:14:10 +0900

    feat: 장바구니에서도 '수량 추가' 가능하도록 업데이트

--- src/components/ReservatedProduct.tsx
+++ src/components/ReservatedProduct.tsx
@@ -7,14 +7,41 @@ import {
   Stack,
   Text,
   Box,
+  useToast,
   Badge,
 } from '@chakra-ui/react';
 import { IProduct } from '@/interface/product';
 import { useAppDispatch } from '@/store';
-import { removeOneFromCart, removeAllFromCart } from '@/store/slices/cartSlice';
+import {
+  addToCart,
+  removeOneFromCart,
+  removeAllFromCart,
+} from '@/store/slices/cartSlice';
 
 const ReservatedProduct = (productData: any) => {
   const dispatch = useAppDispatch();
+  const toast = useToast();
+
+  const handleReservation = (product: any) => {
+    if (product.reservationCount <= Number(product.maximumPurchases)) {
+      dispatch(addToCart(product));
+      toast({
+        title: `${product.name} 1개 추가`,
+        description: `장바구니에 ${product.reservationCount}개 있습니다`,
+        position: 'top-right',
+        status: 'success',
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: `${product.name} 구매 개수 초과`,
+        description: `인 당 ${product.maximumPurchases}개만 구매하실 수 있습니다.`,
+        position: 'top-right',
+        status: 'error',
+        isClosable: true,
+      });
+    }
+  };
 
   return (
     <Card direction={{ base: 'column', sm: 'row' }} w="100%" variant="outline">
@@ -30,6 +57,13 @@ const ReservatedProduct = (productData: any) => {
           </Stack>
         </CardBody>
         <CardFooter gap="5px">
+          <Button
+            variant="solid"
+            colorScheme="blue"
+            onClick={() => handleReservation(productData)}
+          >
+            한개 추가
+          </Button>
           <Button
             variant="solid"
             colorScheme="blue"
